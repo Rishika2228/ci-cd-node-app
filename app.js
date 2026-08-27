72b7501152bf120eab0acbb999508a3f05059826
Rishika Mistri <rishikamistri@gmail.com>
2026-08-27 21:17:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello! CI/CD Node.js Application is Working!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
